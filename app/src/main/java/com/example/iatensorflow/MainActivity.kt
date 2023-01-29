862f50369b6a2f13d05a30f98f7c5fd6689d779a
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2023-01-28 22:17:43 -0300

    configurando os botões

--- app/src/main/java/com/example/iatensorflow/MainActivity.kt
+++ app/src/main/java/com/example/iatensorflow/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.iatensorflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.iatensorflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageView: ImageView
+    private lateinit var button: Button
+    private lateinit var tvOutput: TextView
+    private val GALLERY_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
+
+        imageView = binding.imageview
+        button = binding.btnCaptureImage
+        tvOutput = binding.tvOutput
+        val buttonLoad = binding.btnLoadImage
+
+        button.setOnClickListener {
+
+        }
     }
 }
