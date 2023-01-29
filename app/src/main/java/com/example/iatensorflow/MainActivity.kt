ffa29d259e60868afb062987d51e821bd2bea957
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2023-01-29 20:00:35 -0300

    quae pronto

--- app/src/main/java/com/example/iatensorflow/MainActivity.kt
+++ app/src/main/java/com/example/iatensorflow/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.iatensorflow
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
@@ -16,9 +22,11 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.contentValuesOf
 import com.example.iatensorflow.databinding.ActivityMainBinding
 import com.example.iatensorflow.ml.BirdsModel
 import org.tensorflow.lite.support.image.TensorImage
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(R.layout.activity_main)
+        setContentView(view)
 
         imageView = binding.imageview
         button = binding.btnCaptureImage
@@ -40,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         val buttonLoad = binding.btnLoadImage
 
         button.setOnClickListener {
+
+
             if (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.CAMERA
@@ -53,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonLoad.setOnClickListener {
+            Toast.makeText(this, "Button Load", Toast.LENGTH_LONG).show()
             if (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
@@ -69,6 +80,21 @@ class MainActivity : AppCompatActivity() {
             } else {
                 requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
+        }
+
+        // redireciona para o google usando o nome científico como busca
+        tvOutput.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/search?q=${tvOutput.text}")
+            )
+            startActivity(intent)
+        }
+
+        // baixa a imagem usando longPress
+        imageView.setOnLongClickListener {
+            requestPermissionLaucher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            return@setOnLongClickListener true
         }
 
     }
@@ -135,9 +161,51 @@ class MainActivity : AppCompatActivity() {
 
         // configurando texto de saída
         tvOutput.text = highProbabilityOutput.label
-        Log.i( "TAG", "outputgenerator: $highProbabilityOutput")
+        Log.i("TAG", "outputgenerator: $highProbabilityOutput")
         // Releases model resources if no longer used.
 //        birdsModel.close()
+    }
 
+    private val requestPermissionLaucher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                AlertDialog.Builder(this).setTitle("Baixar imagem?")
+                    .setMessage("Você quer baixar essa imagem no seu dispositivo?")
+                    .setPositiveButton("Sim") { _, _ ->
+                        val drawable: BitmapDrawable = imageView.drawable as BitmapDrawable
+                        val bitmap = drawable.bitmap
+                        downloadImage(bitmap)
+                    }
+                    .setNegativeButton("Não!") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            } else {
+                Toast.makeText(this, "por favor, permita baixar a imagem", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun downloadImage(mBitmap: Bitmap): Uri? {
+        val contentValues = ContentValues().apply {
+            put(
+                MediaStore.Images.Media.DISPLAY_NAME,
+                "Birds_Images" + System.currentTimeMillis() / 1000
+            )
+            put(MediaStore.Images.Media.MIME_TYPE, "images/png")
+        }
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        if (uri != null) {
+            contentResolver.insert(uri, contentValues)?.also {
+                contentResolver.openOutputStream(it).use { outputStream ->
+                    if (!mBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)){
+                        throw IOException("Couldn't save de bitmap")
+                    } else {
+                        Toast.makeText(applicationContext, "imagem salva", Toast.LENGTH_LONG).show()
+                    }
+                }
+                return it
+            }
+        }
+        return null
     }
 }
